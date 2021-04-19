b6d7500f3e790cc8c865e2078326753c11225ba7
Hernan Di Tano <hditano@gmail.com>
2021-04-19 08:13:26 -0300

    added gameBoard and player basic logic

--- script.js
+++ script.js
@@ -1,19 +1,30 @@
 // Main GameBoard
 
 const gameBoard = (() => {
-  let board = ["X", "X", "X", "X", "", "", "", "", ""];
+  let board = ["X", "X", "X", "X", "O", "O", "O", "O", "O"];
 
   const getBoardIndex = (choice) => {
     return board[choice];
   };
 
-  const loopBoard = () => {
-    for(let i = 0; i < board.length; i) {
-      return board[i];
-    }
-  };
+  const setBoard = (index, choice) => {
+    board.splice(index, 1, choice);
+    return board;
+  }
 
-  return {board, getBoardIndex, loopBoard};
+  return {board, getBoardIndex, setBoard};
 })();
 
-console.log(gameBoard.loopBoard);
+// displayController (To render)
+
+const displayController = (() => {
+
+})
+
+// Players Setup
+
+const player = (name) => {
+
+  return { name };
+}
+
