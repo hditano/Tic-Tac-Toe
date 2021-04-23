d5fb54c86dd68bd7aa781c2a764fd4f1d30cab00
Hernan Di Tano <hditano@gmail.com>
2021-04-23 07:47:45 -0300

    fixed checkWinner logic

--- script.js
+++ script.js
@@ -8,7 +8,6 @@ const gameBoard = (() => {
     return board;
   };
 
-
   return { board, setBoard };
 })();
 
@@ -21,11 +20,10 @@ const player = (name) => {
 // displayController (To render)
 
 const displayController = (() => {
-
   const itemSelection = () => {
     document.addEventListener("click", (e) => {
       if (e.target.matches(".item-box")) {
-        if(e.target.textContent !== "") return;
+        if (e.target.textContent !== "") return;
         let dataID = e.target.dataset.id;
         playGame.renderBox(e.target);
         gameBoard.setBoard(dataID, dataID);
@@ -39,32 +37,33 @@ const displayController = (() => {
   return { itemSelection };
 })();
 
+  const winningConditions = (() => {
+    const winingComb = [
+      ['0', '1', '2'],
+      ['3', '4', '5'],
+      ['6', '7', '8'],
+      ['0', '3', '6'],
+      ['1', '4', '7'],
+      ['2', '5', '8'],
+      ['0', '4', '8'],
+      ['2', '4', '6'],
+    ];
 
-const winningConditions = (() => {
+    const checkWinner = () => {
 
-  const winingComb = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
+      console.log(playGame.currentPlayer.Name);
+      
+      
+      for (let i = 0; i < winingComb.length; i++) {
+        let check = winingComb[i].every(function (val) {
+          return playGame.currentPlayer.Moves.indexOf(val) !== -1;
+        });
+        if(check) {
+          console.log('its true by:', playGame.currentPlayer)
+        }
 
-
-  const checkWinner = () => {
-
-    let array1 = [2,4,4,3,2,6];
-
-    for(let i = 0; i < winingComb.length; i++) {
-    let check = winingComb[i].every(function (val) {
-      return array1.indexOf(val) !== -1;
-    })
-    console.log(check);
-  }
-
+      }
+    console.log('current player array', playGame.currentPlayer.Moves, 'typeof', typeof winingComb);
   };
 
   return { checkWinner };
@@ -74,46 +73,57 @@ const playGame = (() => {
   const playerOne = {
     Name: "Hernan",
     Moves: [],
-    Markings: 'X',
+    Markings: "X",
   };
   const playerTwo = {
     Name: "Sebastian",
     Moves: [],
-    Markings: 'O',
+    Markings: "O",
   };
 
   let currentPlayer;
   currentPlayer = playerOne;
 
   const renderHtml = () => {
-    let div = document.querySelector('.board');
-    for(let i = 0; i < gameBoard.board.length; i++) {
-      let para = document.createElement('p');
+    let div = document.querySelector(".board");
+    for (let i = 0; i < gameBoard.board.length; i++) {
+      let para = document.createElement("p");
       let tempNode = document.createTextNode(gameBoard.board[i]);
-      para.setAttribute('data-id', i);
-      para.classList.add('item-box');
+      para.setAttribute("data-id", i);
+      para.classList.add("item-box");
       para.appendChild(tempNode);
       div.appendChild(para);
-
     }
-  }
+  };
 
   const renderBox = (target) => {
-    (currentPlayer === playerOne) ? target.textContent = playerOne.Markings : target.textContent = playerTwo.Markings;
+    currentPlayer === playerOne
+      ? (target.textContent = playerOne.Markings)
+      : (target.textContent = playerTwo.Markings);
   };
 
   const playerMoves = (dataID) => {
-    (currentPlayer === playerOne) ? playerOne.Moves.push(dataID) : playerTwo.Moves.push(dataID);
-    console.log('playerOne', playerOne.Moves, "", 'playerTwo', playerTwo.Moves ); 
-  }
+    currentPlayer === playerOne
+      ? playerOne.Moves.push(dataID)
+      : playerTwo.Moves.push(dataID);
+    console.log("playerOne", playerOne.Moves, "", "playerTwo", playerTwo.Moves);
+  };
 
   const switchPlayer = () => {
-    (currentPlayer === playerOne) ? currentPlayer = playerTwo : currentPlayer = playerOne;
-    console.log(currentPlayer)
+    currentPlayer === playerOne
+      ? (currentPlayer = playerTwo)
+      : (currentPlayer = playerOne);
+    console.log(currentPlayer);
   };
 
   renderHtml();
   displayController.itemSelection(currentPlayer);
-  winningConditions.checkWinner();
-  return { currentPlayer, renderBox, switchPlayer, playerMoves };
+  return {
+    playerOne,
+    playerTwo,
+    currentPlayer,
+    renderBox,
+    switchPlayer,
+    playerMoves,
+  };
 })();
